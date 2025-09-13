9cce43668e9fcf71ab2a45385153fbb44826557d
Aamir-website <doozboi99@gmail.com>
2025-09-13 19:18:41 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -412,7 +412,7 @@ gsap.to(backgroundTextRef.current, {
                 fontFamily: 'IBM Plex Sans, sans-serif',
                 fontWeight: '100',
                 letterSpacing: '0.3em',
-                wordSpacing: '1em',
+                wordSpacing: '5em',
                 animationDelay: '0.8s', 
                 animationFillMode: 'forwards',
                 textShadow: '0 10px 20px rgba(0,0,0,0.3)'
