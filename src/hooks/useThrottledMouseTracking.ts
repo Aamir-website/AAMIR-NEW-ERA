b43e635e6ab786c5e8b17ded5b7f40cdb34158b5
Aamir-website <doozboi99@gmail.com>
2025-09-13 18:30:39 +0530

    Updated useThrottledMouseTracking.ts

--- src/hooks/useThrottledMouseTracking.ts
+++ src/hooks/useThrottledMouseTracking.ts
@@ -22,6 +22,10 @@ export function useThrottledMouseTracking(isEnabled: boolean = true) {
         if (isCursorInsideHero) {
           const x = (e.clientX / window.innerWidth - 0.5) * 1.5;
           const y = (e.clientY / window.innerHeight - 0.7) * 0.2;
+            // Apply tint/offset to match your eye alignment
+    const tintX = 0.05; // adjust left/right
+    const tintY = -0.02; // adjust up/down
+
           setMousePosition({ x, y });
         }
       });
