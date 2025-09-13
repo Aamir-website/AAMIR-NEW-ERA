05c5b66717f72c91f8bfd8636d0de587aea3ded5
Aamir-website <doozboi99@gmail.com>
2025-09-13 19:37:20 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -473,7 +473,7 @@ gsap.to(backgroundTextRef.current, {
       z-0 
       pointer-events-none 
       overflow-hidden 
-      transform -translate-y-20
+      transform -translate-y-40
     "
   >
             <div className="hidden md:block">
