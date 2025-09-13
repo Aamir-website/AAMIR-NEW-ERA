bc83d0931c99d8a5bea7de88fe22f654b57d2cd3
Aamir-website <doozboi99@gmail.com>
2025-09-13 19:26:41 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -463,7 +463,7 @@ gsap.to(backgroundTextRef.current, {
 
         {/* Floating Testimonial Badges */}
         {showTestimonials && (
-          <div className="fixed inset-0 z-0 pointer-events-none overflow-hidden transform -translate-y-40">
+          <div className="fixed inset-0 z-0 pointer-events-none overflow-hidden transform -translate-y-40 scale-90">
             <div className="hidden md:block">
               {testimonialBadges.map((badge, index) => (
                 <TestimonialBadge key={index} badge={badge} />
