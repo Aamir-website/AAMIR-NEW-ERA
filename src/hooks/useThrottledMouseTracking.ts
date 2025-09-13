98ee4ee0b420448525254d464c786b8e0ef038b3
Aamir-website <doozboi99@gmail.com>
2025-09-13 18:24:14 +0530

    Updated useThrottledMouseTracking.ts

--- src/hooks/useThrottledMouseTracking.ts
+++ src/hooks/useThrottledMouseTracking.ts
@@ -21,7 +21,7 @@ export function useThrottledMouseTracking(isEnabled: boolean = true) {
       rafRef.current = requestAnimationFrame(() => {
         if (isCursorInsideHero) {
           const x = (e.clientX / window.innerWidth - 0.5) * 1.5;
-          const y = (e.clientY / window.innerHeight - 0.5) * 0.2;
+          const y = (e.clientY / window.innerHeight - 0) * 0.2;
           setMousePosition({ x, y });
         }
       });
